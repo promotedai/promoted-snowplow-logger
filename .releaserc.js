d5239cee3a75a8e1df97894877bbbb65e576c7f7
Dan Hill <dhill@promoted.ai>
2024-06-29 08:23:47 -0700

    switch more to ES modules

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   branch: 'main',
   plugins: [
     '@semantic-release/commit-analyzer',
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' }, modules: false, loose: true }],
     '@babel/preset-typescript',
